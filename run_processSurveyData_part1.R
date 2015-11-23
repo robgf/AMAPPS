e37984c2217a29c7a7de786b39734704187b6410
Kaycee Coleman <kaycee.coleman@gmail.com>
2015-11-23 12:58:04 -0500

    Create run_processSurveyData_part1.R
    create run part 1 script

---
+++ run_processSurveyData_part1.R
@@ -0,0 +1,36 @@
+## created by J. Leirness, July 2015
+## edited by K. Coleman, July 2015
+
+# LOAD PACKAGES 
+require(RODBC) # used in ObsFilesErrorCheck function to import excel species codes
+require(geosphere) # used in fixSeconds function
+require(parallel) # used to make a cluster
+require(rgdal) # for writeOGR
+require(zoo) # fill in missing points
+
+# DEFINE SURVEY, CHANGE THIS!!!
+surveyFolder = "AMAPPS/"
+yearLabel = "AMAPPS_2013_09"
+
+# SET INPUT/OUTPUT DIRECTORY PATHS
+dir <- "//IFW9MBMSVR010/SeaDuck/NewCodeFromJeff_20150720/Jeff_Working_Folder/"
+setwd(dir)
+dbpath <- gsub("Jeff_Working_Folder", "DataBase", dir)
+dir.in <- paste("//IFW9MBMSVR010/SeaDuck/NewCodeFromJeff_20150720/SurveyData/", surveyFolder, yearLabel, sep = "") 
+dir.out <- paste("//IFW9MBMSVR010/SeaDuck/NewCodeFromJeff_20150720/Jeff_Working_Folder/DataProcessing/Surveys/", surveyFolder, yearLabel, sep = "") 
+speciesPath = "//IFW9MBMSVR010/SeaDuck/NewCodeFromJeff_20150720/Jeff_Working_Folder/DataProcessing/"
+
+# SOURCE R FUNCTIONS
+source(file.path(dir, "_Rfunctions/sourceDir.R"))
+sourceDir(file.path(dir, "_Rfunctions"))
+
+# SET PATH TO R FILE THAT FIXES DATA ERRORS
+errfix.file <- file.path(dir.out, paste(yearLabel, "_ObsFilesFix.R", sep = ""))
+
+# SET PATH TO python.exe FILE
+py.exe = "E:/Python27/ArcGISx6410.2/python.exe"
+
+# RUN EDIT FUNCTION
+source("//IFW9MBMSVR010/SeaDuck/NewCodeFromJeff_20150720/Jeff_Working_Folder/DataProcessing/Code/processSurveyData_part1.R")
+
+processSurveyData_part1(dir.in, dir.out, errfix.file, py.exe = "'C:/Program Files (x86)/ArcGIS/ArcGIS 10.2/python.exe'")
