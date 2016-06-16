9d829fe2b63075b700e9437a63806ca298b3383a
Kaycee Coleman <kaycee.coleman@gmail.com>
2016-06-16 17:12:32 -0400

    Update getData.R

--- NWASC/cleaning/functions/getData.R
+++ NWASC/cleaning/functions/getData.R
@@ -13,22 +13,25 @@ getData <- function(data.path) {
   ncols <- max(count.fields(data.path, sep = ",", quote = NULL))
   obs.i <- read.csv(data.path, header = TRUE,  
                   colClasses = "character")
+  if(dirname(data.path)=="\\\\IFW9mbm-fs1/SeaDuck/seabird_database/datasets_received/MassCEC/MCEC_Year2_Seabirds_9132013/MCEC_Year2_Seabirds_9132013" &
+       ncols==3) {colnames(obs.i)=c("lat","lon","sec")}
+  
   # CREATE DISTANCE COLUMN IF THERE ISNT ONE
   if(all(!names(obs.i) %in% "distance")) {obs.i$distance=""}
   # FIX COLUMN NAMES
   if(any(names(obs.i) %in% "number_")){names(obs.i)[names(obs.i) == "number_"] = "number"}
   if(any(names(obs.i) %in% "time_")){names(obs.i)[names(obs.i) == "time_"] = "time"}
-  if(any(names(obs.i) %in% "dist")){names(obs.i)[names(obs.i) == "dist"] = "distance"}
-  if(any(names(obs.i) %in% "distance.1")){names(obs.i)[names(obs.i) == "distance.1"] = "distance"}
+  if(any(names(obs.i) %in% "OBJECTID")){names(obs.i)[names(obs.i) == "OBJECTID"] = "objectid"}
+  if(any(names(obs.i) %in% c("dist","distance.1"))){names(obs.i)[names(obs.i) %in% c("dist","distance.1")] = "distance"}
   if(any(names(obs.i) %in% "year_")){names(obs.i)[names(obs.i) == "year_"] = "year"}
   if(any(names(obs.i) %in% "month_")){names(obs.i)[names(obs.i) == "month_"] = "month"}
   if(any(names(obs.i) %in% "day_")){names(obs.i)[names(obs.i) == "day_"] = "day"}
+  if(any(names(obs.i) %in% "null_")){names(obs.i)[names(obs.i) == "null_"] = "null"}
   if(any(names(obs.i) %in% "position_")){names(obs.i)[names(obs.i) == "position_"] = "position"}
   if(any(names(obs.i) %in% "trans")){names(obs.i)[names(obs.i) == "trans"] = "transect"}
   if(any(names(obs.i) %in% "conditons")){names(obs.i)[names(obs.i) == "conditons"] = "conditions"}
-  if(any(names(obs.i) %in% "comment")){names(obs.i)[names(obs.i) == "comment"] = "comments"}
+  if(any(names(obs.i) %in% c("comment","comment_"))){names(obs.i)[names(obs.i) %in% c("comment","comment_")] = "comments"}
   if(any(names(obs.i) %in% "beahvior")){names(obs.i)[names(obs.i) == "beahvior"] = "behavior"}
-  
   
   # CREATE FILE COLUMN
   obs.i$file <- data.path
