7ce80f5c85c3135d32e007f538c0f4617729777b
Kaycee Coleman <kaycee.coleman@gmail.com>
2016-08-08 17:16:32 -0400

    Update BOEM_HiDefStudy_ObsFilesFix.R

--- NWASC/cleaning/BOEM_HiDef_NC/BOEM_HiDefStudy_ObsFilesFix.R
+++ NWASC/cleaning/BOEM_HiDef_NC/BOEM_HiDefStudy_ObsFilesFix.R
@@ -1,7 +1,26 @@
-# --------------------------------------------- #
-#                     STEP 1                    #
-#          FIX INCORRECT TYPE CODINGS           #
-# --------------------------------------------- #
+# --------------------------- #
+# species errors
+# fix incorrect type codings
+# --------------------------- #
+
+## Species Information
+# edit the table to consolidate
+Species_Information$scientific_name = as.character(Species_Information$scientific_name)
+Species_Information$scientific_name[is.na(Species_Information$scientific_name) & !is.na(Species_Information$species)] = 
+  as.character(Species_Information$species[is.na(Species_Information$scientific_name) & !is.na(Species_Information$species)])
+Species_Information$common_name = as.character(Species_Information$common_name)
+Species_Information$commonc_name[is.na(Species_Information$common_name) & !is.na(Species_Information$common_name.1)] = 
+  as.character(Species_Information$common_name.1[is.na(Species_Information$common_name) & !is.na(Species_Information$common_name.1)])
+drops <- c("french_name","E","I","H","A","N","M","species","ID", "common_name.1")
+Species_Information = Species_Information[,!(names(Species_Information) %in% drops)]
+
+common_name = tolower(Species_Information$common_name)
+spplist = as.character(Species_Information$species_code)
+
+# list of codes we are using
+codes = odbcConnectExcel2007(file.path(dbpath, "NWASC_codes.xlsx"), readOnly = TRUE) 
+name <- sqlFetch(codes,"codes")
+odbcCloseAll()
 
 #species_code
 spplist = ifelse(common_name == "humpback whale", "HUWH", spplist)
@@ -145,3 +164,126 @@ spplist = toupper(spplist)
 # calidris species", "UNCA"
 
 # might need to split up Thalasseus and Sterna (UNTE)
+
+fieldData$species = tolower(fieldData$species)
+
+fieldData = data.frame(cbind(species = common_name, spp_type = spplist), stringsAsFactors = FALSE) %>% distinct %>%
+  left_join(fieldData, ., by = "species") %>% rowwise %>% mutate(type = replace(type, is.na(type), spp_type)) %>%
+  select(-spp_type) %>% as.data.frame
+rm(common_name,spplist, Species_Information, name)
+
+# fixed mixed
+toAdd = fieldData[fieldData$ID %in% c("3059"),] 
+toAdd$ID = 3059.5 
+toAdd$type = "BODO"
+toAdd$species = "bottlenose dolphin"
+toAdd$behavior = ""
+toAdd$number_individuals = 1
+toAdd$comments = "added BODO from comments of ID 3059"
+fieldData = rbind(fieldData, toAdd); rm(toAdd)
+# --------------------------- #
+
+# --------------------------- #
+# BEG and END errors
+# --------------------------- #
+
+# errors (some were in the middle of transects, rather than at the end)
+fieldData$type[fieldData$ID %in% c("590","678","702","710","717","725","731","735",
+                                   "733","855","901","999","1060","1076","1191",
+                                   "1213", "1297","2873","2911")] = "ENDCNT"
+fieldData$type[fieldData$ID %in% c("679","703","711","718","726","732","736","856",
+                                   "902","910","1000","1061","1298","1360","2925")] = "BEGCNT"
+fieldData$type[fieldData$ID %in% c("1788","2878","2881","2882","2953","3009","3032",
+                                   "3033","3034","3035","3036","3037","3038","3039")] = "COMMENT"
+# contact provider about ID 3009 (Chon), missing_sp marked yes
+
+# add begin count for pause in transect
+toAdd = fieldData[fieldData$ID %in% c("2874"),] 
+toAdd$ID = 2873.5     
+toAdd$type = "BEGCNT"
+toAdd$species = ""
+toAdd$behavior = ""
+toAdd$number_individuals = ""
+toAdd$comments = "added BEGCNT after transect pause"
+fieldData = rbind(fieldData, toAdd); rm(toAdd)
+
+toAdd = fieldData[fieldData$ID %in% c("2912"),] 
+toAdd$ID = 2911.5 
+toAdd$type = "BEGCNT"
+toAdd$species = ""
+toAdd$behavior = ""
+toAdd$number_individuals = ""
+toAdd$comments = "added BEGCNT after transect pause"
+fieldData = rbind(fieldData, toAdd); rm(toAdd)
+
+toAdd = fieldData[fieldData$ID %in% c("3032"),] 
+toAdd$ID = 3031.5 
+toAdd$type = "BEGCNT"
+toAdd$species = ""
+toAdd$behavior = ""
+toAdd$number_individuals = ""
+toAdd$comments = "added BEGCNT for chum flyover"
+fieldData = rbind(fieldData, toAdd); rm(toAdd)
+
+toAdd = fieldData[fieldData$ID %in% c("3049"),] 
+toAdd$ID = 3049.5 
+toAdd$type = "ENDCNT"
+toAdd$species = ""
+toAdd$behavior = ""
+toAdd$number_individuals = ""
+toAdd$comments = "added ENDCNT for chum flyover"
+fieldData = rbind(fieldData, toAdd); rm(toAdd)
+
+# --------------------------- #
+## GPS
+names(GPSdata)[names(GPSdata) == "/trk/trkseg/trkpt/@lat"] = "lat"
+names(GPSdata)[names(GPSdata) == "/trk/trkseg/trkpt/@lon"] = "long"
+names(GPSdata)[names(GPSdata) == "/trk/trkseg/trkpt/time"] = "date"
+GPSdata$platform = tolower(GPSdata$platform)
+# --------------------------- # 
+
+# --------------------------- #
+# time
+fieldData$year_[fieldData$year_ == 11] = 2011
+# fix time stamps and 
+# add lat and long to observations
+df <- data.frame(date = fieldData$obs_time_rd, #satellite_GPS_time,
+                 hr = as.numeric(format(fieldData$obs_time_rd, format = "%H")),
+                 min = as.numeric(format(fieldData$obs_time_rd, format = "%M")),
+                 sec = as.numeric(format(fieldData$obs_time_rd, format = "%S")))
+fieldData$date = ISOdatetime(fieldData$year_, fieldData$month_, fieldData$day, df$hr, df$min, df$sec) #Y m d H M S
+rm(df)
+#df <- data.frame(date = GPSdata$GPS_time_rd,
+#                 hr = as.numeric(format(GPSdata$GPS_time_rd , format = "%H")),
+#                 min = as.numeric(format(GPSdata$GPS_time_rd , format = "%M")),
+#                 sec = as.numeric(format(GPSdata$GPS_time_rd , format = "%S")))
+#GPSdata$date = ISOdatetime(GPSdata$year_, GPSdata$month_, GPSdata$day, df$hr, df$min, df$sec) #Y m d H M S
+#rm(df)
+# --------------------------- # 
+
+# --------------------------- # 
+# CREW
+fieldData$crew = sub("^\\s+", "", tolower(paste(fieldData$obs_first_name, fieldData$obs_last_name, sep = "_")))
+fieldData$crew[fieldData$crew == "allison_mac connell"] = "allison_macconnell"
+fieldData$crew[fieldData$crew == "mary jo_barkaszi"] = "maryjo_barkaszi"
+fieldData$crew[fieldData$crew == "na_na"] = NA
+#fieldData$crew[fieldData$crew == "erik_haney"] = "eric_haney" # probably???????????????????????
+fieldData = fieldData %>% select(-obs_first_name, -obs_last_name)
+# --------------------------- # 
+
+# --------------------------- # 
+# platform
+fieldData$platform = tolower(fieldData$platform)
+# --------------------------- # 
+
+# --------------------------- # 
+# count
+names(fieldData)[names(fieldData) == "number_individuals"] = "count"
+# --------------------------- # 
+
+# --------------------------- # 
+# remove unneccessary fields
+fieldData = fieldData %>% select(-Observers, missing_sp, -F26,-obs_time_rd,
+                                 -cue_type_start_stop,-start_time_sheet,
+                                 -end_time_sheet,-page_number,-page_total)
+#
