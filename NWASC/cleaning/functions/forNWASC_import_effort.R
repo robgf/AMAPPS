e3fa095fe6b1e29a54b49b42ca4a067ff1848c90
Kaycee Coleman <kaycee.coleman@gmail.com>
2016-09-16 18:44:33 -0400

    Update forNWASC_import_effort.R

--- NWASC/cleaning/functions/forNWASC_import_effort.R
+++ NWASC/cleaning/functions/forNWASC_import_effort.R
@@ -1,14 +1,19 @@
 # ------------------------------- #
-# track = pieces
-# transect = 1
+# This script is to format track and transect data and 
+# to import it into a temporary database to be evaluated
+#
+# run forNWASC_import_obstbl first so that we can then add the transect_id numbers to the observations
+# matching by date
 # ------------------------------- #
 
-forNWASC_import_effort <- function(id, data_track, data_transect) {
+forNWASC_import_effort <- function(id, data_track, data_transect, estimated_effort_turning_points) {
   # load dataset descriptions
   library(RODBC)
   db <- odbcConnectAccess2007("//IFW9mbm-fs1/SeaDuck/seabird_database/data_import/in_progress/NWASC_temp.accdb")
   transects.in.db = sqlFetch(db, "transect")
   tracks.in.db = sqlFetch(db, "track")
+  data.in.db = sqlFetch(db, "dataset")
+  obs.in.db = sqlFetch(db, "observation")
   
   # ------------------------ #
   # transect
@@ -55,6 +60,27 @@ forNWASC_import_effort <- function(id, data_track, data_transect) {
   dat_track$track_dt = format(as.Date(data_track[,which(colnames(data_track) %in% c("date","start_dt","start_date","gps_date"))]),'%m/%d/%Y')
   dat_track$source_transect_id = data_track[,which(colnames(data_track) %in% c("transect","transect_id"))]
   dat_track$source_track_id = data_track[,which(colnames(data_track) %in% c("index"))]
+  dat_track$track_id = c((max(tracks.in.db$track_id)+1):(max(tracks.in.db$track_id)+dim(data_track)[1]))
+  
+  dat_track = dat_track %>% mutate(track_id = as.numeric(track_id),           
+                       track_dt = as.character(track_dt),           
+                       track_tm = as.character(track_tm),           
+                       track_lat = as.numeric(track_lat),                
+                       track_lon = as.numeric(track_lon),               
+                       point_type = as.character(point_type),          
+                       source_survey_id = as.character(source_survey_id),    
+                       source_transect_id = as.character(source_transect_id),  
+                       observer_position = as.character(observer_position),   
+                       observer = as.character(observer),           
+                       offline = as.numeric(offline),                  
+                       seastate = as.character(seastate),           
+                       comment = as.character(comment),            
+                       transect_id = as.numeric(transect_id),             
+                       dataset_id = as.numeric(dataset_id),              
+                       track_gs = as.character(track_gs),            
+                       piece = as.numeric(piece),                 
+                       source_track_id = as.numeric(source_track_id))
+  
   
   # if the transect information needs to be pulled from the track files
   if(missing(data_transect)) {
@@ -71,7 +97,12 @@ forNWASC_import_effort <- function(id, data_track, data_transect) {
       arrange(point_type) %>%
       summarize(start_lon = first(track_lon), start_lat = first(track_lat), end_lon = last(track_lon), end_lat = last(track_lat)) %>%
       rowwise %>% 
-      mutate(distance =  distm(c(start_lat, start_lon), c(end_lat, end_lon), fun = distHaversine)) %>% ungroup 
+      mutate(distance =  distm(c(start_lat, start_lon), c(end_lat, end_lon), fun = distHaversine)) %>% 
+      ungroup %>% as.data.frame
+    
+    #estimated_effort_turning_points
+    
+    
     #summarize pieces to transects
     transects = transect_pieces %>% 
       mutate(source_transect_id = factor(source_transect_id)) %>% 
@@ -84,8 +115,71 @@ forNWASC_import_effort <- function(id, data_track, data_transect) {
     colnames(dat_transect) = colnames(transects.in.db)
     same_nm = colnames(transects[colnames(transects) %in% colnames(dat_transect)])
     dat_transect[,same_nm] = transects[,same_nm]
+    dat_transect$dataset_id = id
+    dat_transect$transect_id = c((max(transects.in.db$transect_id)+1):(max(transects.in.db$transect_id)+dim(dat_transect)[1]))
+    dat_transect$source_dataset_id = as.character(data.in.db$source_dataset_id[data.in.db$dataset_id==id])
+    
+    
+    dat_transect = dat_transect %>% 
+      mutate(temp_start_lat = transect_pieces$start_lat[transect_pieces$track_dt == start_dt]) %>%
+      mutate(temp_start_lon = transect_pieces$start_lon[transect_pieces$track_dt == start_dt]) %>%
+      mutate(temp_stop_lat = transect_pieces$end_lat[transect_pieces$track_dt == end_dt]) %>%
+      mutate(temp_stop_lat = transect_pieces$end_lon[transect_pieces$track_dt == end_dt])
+
+    dat_transect = dat_transect %>% mutate(transect_id = as.numeric(transect_id),
+                                           dataset_id = as.numeric(dataset_id),
+                                           source_transect_id = as.character(source_transect_id),
+                                           source_dataset_id = as.character(source_dataset_id),
+                                           start_dt = as.character(start_dt),
+                                           start_tm = as.character(start_tm),
+                                           end_dt = as.character(end_dt),
+                                           end_tm = as.character(end_tm),
+                                           transect_time_min_nb = as.numeric(transect_time_min_nb),
+                                           transect_distance_nb = as.numeric(transect_distance_nb),
+                                           traversal_speed_nb = as.numeric(traversal_speed_nb),
+                                           transect_width_nb = as.numeric(transect_width_nb),
+                                           observers_tx = as.character(observers_tx),
+                                           visability_tx = as.character(visability_tx),
+                                           weather_tx = as.character(weather_tx),
+                                           seastate_beaufort_nb = as.numeric(seastate_beaufort_nb),
+                                           wind_speed_tx = as.character(wind_speed_tx),
+                                           wind_dir_tx = as.character(wind_dir_tx),
+                                           seasurface_tempc_nb = as.numeric(seasurface_tempc_nb),
+                                           comments_tx = as.character(comments_tx),
+                                           track_gs = as.character(track_gs),
+                                           conveyance_name_tx = as.character(conveyance_name_tx),
+                                           heading_tx = as.character(heading_tx),
+                                           wave_height_tx = as.character(wave_height_tx),
+                                           spatial_type_tx = as.character(spatial_type_tx),
+                                           who_created = as.character(who_created),
+                                           date_created = as.character(date_created),
+                                           utm_zone = as.character(utm_zone),
+                                           whole_transect = as.character(whole_transect),
+                                           local_transect_id = as.character(local_transect_id),
+                                           who_imported = as.character(who_imported),
+                                           temp_start_lat = as.numeric(temp_start_lat),
+                                           temp_start_lon = as.numeric(temp_start_lon),
+                                           temp_stop_lat = as.numeric(temp_stop_lat),
+                                           temp_stop_lon = as.numeric(temp_stop_lon),
+                                           obs_position = as.character(obs_position),
+                                           visit = as.character(visit),
+                                           time_from_midnight_start = as.character(time_from_midnight_start),
+                                           time_from_midnight_stop = as.character(time_from_midnight_stop),
+                                           date_imported = as.character(date_imported),
+                                           local_survey_id = as.character(local_survey_id),
+                                           local_transect_id2 = as.character(local_transect_id2),
+                                           survey_type = as.character(survey_type),
+                                           temp_end_lat = as.character(temp_end_lat))
     
  }
+  
+  # ------------------------ #
+  # join transect numbers to both track and observations tables based on date
+  dat_transect = dat_transect %>% 
+    mutate(temp_start_lat = transect_pieces$start_lat[transect_pieces$track_dt == start_dt]) %>%
+  # ------------------------ #
+  
+  
   # ------------------------ #
   
     
