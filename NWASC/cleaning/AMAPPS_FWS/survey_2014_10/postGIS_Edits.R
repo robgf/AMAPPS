8656639eeb13483ec3d298d01fb62781d38e04b4
Kaycee Coleman <kaycee.coleman@gmail.com>
2017-02-24 18:17:26 -0500

    Create postGIS_Edits.R

---
+++ NWASC/cleaning/AMAPPS_FWS/survey_2014_10/postGIS_Edits.R
@@ -0,0 +1,15 @@
+# ---------------- #
+# post GIS edits 
+# ---------------- #
+
+# wrong transect but distance error was not thrown 
+shapefileDataframe$transect[shapefileDataframe$index %in% c(2183:2185)] = 442602
+
+#right on the edge of land/ could be either since no start/stops
+shapefileDataframe$offline[shapefileDataframe$index == 126298] = 0 
+
+# wrong transect not fixed in check 
+shapefileDataframe$transect[shapefileDataframe$ID %in% c(3212:3343, 33065,33066)] = 350100
+shapefileDataframe$type[shapefileDataframe$ID %in% 3212] = "BEGSEG"
+
+shapefileDataframe$transect[shapefileDataframe$ID %in% c(30160:30162)] = 352100
