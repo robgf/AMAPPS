679a37fddd7d2e73201be44327dc823e1475bc87
Kaycee Coleman <kaycee.coleman@gmail.com>
2017-03-02 17:26:05 -0500

    Update AMAPPS_2014_10_formatting.R

--- NWASC/cleaning/AMAPPS_FWS/survey_2014_10/AMAPPS_2014_10_formatting.R
+++ NWASC/cleaning/AMAPPS_FWS/survey_2014_10/AMAPPS_2014_10_formatting.R
@@ -25,9 +25,9 @@ dir.out <- paste(gsub("datasets_received", "data_import/in_progress", dir), surv
 #---------------------#
 # load data
 #---------------------#
-transect = read.csv(file.path(dir.in, ".csv"), header = TRUE)
-track = read.csv(file.path(dir.in, ".csv"), header = TRUE)
-obs = read.csv(file.path(dir.in, ".csv"), header = TRUE)
+transect = read.csv(file.path(dir.in, "AMAPPS_2014_10_TransectTbl.csv"), header = TRUE)
+track = read.csv(file.path(dir.in, "AMAPPS_2014_10_Tracks.csv"), header = TRUE)
+obs = read.csv(file.path(dir.in, "AMAPPS_2014_10_Observations.csv"), header = TRUE)
 #---------------------#
 
 
@@ -41,25 +41,23 @@ track = track[!track$type %in% "COCH",]
 #---------------------#
 # obs formatting
 #---------------------#
-obs = obs %>% mutate(Condition = replace(Condition, Condition==1, "1 - worst observation conditions"),
-                     Condition = replace(Condition, Condition==2, "2 - bad observation conditions"), 
-                     Condition = replace(Condition, Condition==3, "3 - average observation conditions"),
-                     Condition = replace(Condition, Condition==4, "4 - good observation conditions"),
-                     Condition = replace(Condition, Condition==5, "5 - excellent observation conditions"))
-      rename(source_transect_id = Transect, weather_tx = Condition, obs_position = Seat,
-      seconds_from_midnight_nb = Sec)
-obs = mutate(obs, source_transect_id = paste(source_transect_id, Obs, sep="_"))
-obs$obs_dt = as.POSIXct(paste(obs$Year, obs$Month, obs$Day, sep="/"), format="%Y/%m/%d")
+obs = obs %>% 
+  rename(source_transect_id = transect, obs_position = seat, seconds_from_midnight_nb = Time_secs,
+         spp_cd = type, temp_lat = latitude_dd, temp_lon = longitude_dd, source_obs_id = ID) %>%
+  mutate(original_species_tx = spp_cd, 
+         source_transect_id = paste(source_transect_id, obs, day, sep="_"),
+         obs_dt = as.POSIXct(paste(year, month, day, sep="/"), format="%Y/%m/%d"))
 #---------------------#
 
 
 #---------------------#
 # track formating
 #---------------------#
-track = rename(track, source_transect_id = Transect,  
-               observer_position = Seat, seconds_from_midnight_nb = Sec, observer = Obs)
-track = mutate(track, source_transect_id = paste(source_transect_id, Obs, sep="_"))
-track$track_dt = as.POSIXct(paste(track$Year, track$Month, track$Day, sep="/"), format="%Y/%m/%d")
-track$type[track$type %in% "BEGSEG"] = "BEGCNT"
-track$type[track$type %in% "ENDSEG"] = "ENDCNT"
+track = track %>%
+  rename(source_transect_id = transect, observer_position = seat, seconds_from_midnight_nb = sec, 
+         observer = obs) %>%
+  mutate(source_transect_id = paste(source_transect_id, obs, day, sep="_"),
+         track_dt = as.POSIXct(paste(year, month, day, sep="/"), format="%Y/%m/%d"))#,
+         #type = replace(type, type=="BEGTRAN","BEGCNT"),
+         #type = replace(type, type=="ENDTRAN","ENDCNT"))
 #---------------------#
