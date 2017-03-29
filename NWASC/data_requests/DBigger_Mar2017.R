548f2a061b078222736665575e16d539ad61f399
Kaycee Coleman <kaycee.coleman@gmail.com>
2017-03-29 17:36:34 -0400

    Update DBigger_Mar2017.R

--- NWASC/data_requests/DBigger_Mar2017.R
+++ NWASC/data_requests/DBigger_Mar2017.R
@@ -98,6 +98,83 @@ old_transect_points = old_transect_points %>%
 
 
 # ------------------------ #
+# fix archived observation species codes
+# ------------------------ #
+db <- odbcConnectAccess2007("//IFW9mbm-fs1/SeaDuck/seabird_database/data_import/in_progress/NWASC_temp.accdb")
+spplist <- sqlFetch(db, "lu_species")
+odbcClose(db)
+
+tmp <- !old_obs$spp_cd %in% spplist$spp_cd
+message("Found ", sum(tmp), " entries with non-matching AOU codes")
+sort(unique(old_obs$spp_cd[tmp]))
+rm(spplist)
+
+old_obs$spp_cd[old_obs$spp_cd %in% c(" ATLANTIC")] = "ATPU"              
+old_obs$spp_cd[old_obs$spp_cd %in% c(" Balaenidae")] = "UNLW"
+old_obs$spp_cd[old_obs$spp_cd %in% c(" BLACK") & old_obs$original_species_tx %in% c("SCOTER")] = "BLSC" 
+old_obs$spp_cd[old_obs$spp_cd %in% c(" BLACK") & old_obs$original_species_tx %in% c("GUILLEMOT")] = "BLGU"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" blackfish")] = "SFWH"              
+old_obs$spp_cd[old_obs$spp_cd %in% c(" BLK-LEGGD")] = "BLKI"             
+old_obs$spp_cd[old_obs$spp_cd %in% c(" BROWN")] = "BRPE"                  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" COMMON") & old_obs$original_species_tx %in% c("LOON")] = "COLO"
+old_obs$spp_cd[old_obs$spp_cd %in% c(" COMMON") & old_obs$original_species_tx %in% c("EIDER")] = "COEI" 
+old_obs$spp_cd[old_obs$spp_cd %in% c(" CORY'S")] = "COSH"                 
+old_obs$spp_cd[old_obs$spp_cd %in% c(" DBL CREST")] = "DCCO"              
+old_obs$spp_cd[old_obs$spp_cd %in% c(" FBSP)")] = "UNFR"                 
+old_obs$spp_cd[old_obs$spp_cd %in% c(" goose-beaked whale")] = "CBWH"     
+old_obs$spp_cd[old_obs$spp_cd %in% c(" gray grampus")] = "RIDO"           
+old_obs$spp_cd[old_obs$spp_cd %in% c(" GREAT")] = "GRCO"                  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" GREAT BLK-BACK")] = "GBBG"         
+old_obs$spp_cd[old_obs$spp_cd %in% c(" GREATER")] = "GRSH"               
+old_obs$spp_cd[old_obs$spp_cd %in% c(" GUGE")] = "UNGU"                   
+old_obs$spp_cd[old_obs$spp_cd %in% c(" HERRING")] = "HERG"                
+old_obs$spp_cd[old_obs$spp_cd %in% c(" HORNED")] = "HOGR"                
+old_obs$spp_cd[old_obs$spp_cd %in% c(" Lagenorhynchus")] = "UNDO"         
+old_obs$spp_cd[old_obs$spp_cd %in% c(" LESS BLK-BACK")] = "LBBG"         
+old_obs$spp_cd[old_obs$spp_cd %in% c(" LOC3)")] = "UNKN"                  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" MANX")] = "MASH"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("LOON")] = "UNLO"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("BIRD")] = "UNBI"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("CORMORANT")] = "UNCO"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("GREBE")] = "UNGR"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("GULL")] = "UNGU"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("MERGANSER")] = "UNME"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("MURRE")] = "UNMU"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("SHEARWATER")] = "UNSH"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("TERN")] = "UNTE"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NK") & old_obs$original_species_tx %in% c("STORM PETREL")] = "UNSP"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NORTHERN") & old_obs$original_species_tx %in% c("GANNET")] = "NOGA"      
+old_obs$spp_cd[old_obs$spp_cd %in% c(" NORTHERN") & old_obs$original_species_tx %in% c("FULMAR")] = "NOFU"  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" Orca or Globicephala)")] = "UNWH" 
+old_obs$spp_cd[old_obs$spp_cd %in% c(" RED-THROATED")] = "RTLO"           
+old_obs$spp_cd[old_obs$spp_cd %in% c(" SOOTY")] = "SOSH"                  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" spotter porpoise")] = "ASDO"       
+old_obs$spp_cd[old_obs$spp_cd %in% c(" streaker porpoise")] = "STDO"   
+old_obs$spp_cd[old_obs$spp_cd %in% c(" PEBL)")] = "UNSP"
+old_obs$spp_cd[old_obs$spp_cd %in% c(" TERN)")] = "UNTE"                  
+old_obs$spp_cd[old_obs$spp_cd %in% c(" THICK-BILLED")] = "TBMU"           
+old_obs$spp_cd[old_obs$spp_cd %in% c(" THIN-BILLED")] = "COMU"            
+old_obs$spp_cd[old_obs$spp_cd %in% c(" WHITE-WINGED")] = "WWSC"           
+old_obs$spp_cd[old_obs$spp_cd %in% c(" WILSON")] = "WISP"                
+old_obs$spp_cd[old_obs$spp_cd %in% c("CRTE")] = "UCRT"                    
+old_obs$spp_cd[old_obs$spp_cd %in% c("TEAL")] = "UNTL"                    
+old_obs$spp_cd[old_obs$spp_cd %in% c("UNPI")] = "UNSE"   
+#old_obs$spp_cd[old_obs$spp_cd %in% c("GLSP")] = ""  
+
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("LOON")] = "UNLO"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("BIRD")] = "UNBI"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("CORMORANT")] = "UNCO"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("GREBE")] = "UNGR"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("GULL")] = "UNGU"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("MERGANSER")] = "UNME"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("MURRE")] = "UNMU"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("SHEARWATER")] = "UNSH"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("TERN")] = "UNTE"  
+old_obs$spp_cd[old_obs$spp_cd %in% c("") & old_obs$original_species_tx %in% c("STORM PETREL")] = "UNSP" 
+# ------------------------ #
+
+
+# ------------------------ #
 # combine datasets
 # ------------------------ #
 obs.in.db$heading_tx = as.character(obs.in.db$heading_tx)
