0af4da78d87b8c2d70f7772e59c66d012371d69c
Kaycee Coleman <kaycee.coleman@gmail.com>
2017-07-17 12:08:47 -0400

    Update MTJones_FWS_July2017.R

--- NWASC/data_requests/MTJones_FWS_July2017.R
+++ NWASC/data_requests/MTJones_FWS_July2017.R
@@ -1,5 +1,3 @@
-# in progress...
-
 # ------------------------ #
 # combine old and new seabird catalog data
 # pull FWS AMAPPS and seaduck data
@@ -48,25 +46,23 @@ dir.from.segmentation = "//IFW9mbm-fs1/SeaDuck/Kyle_Working_Folder/Segmentation/
 # and transects
 # ------------------------ #
 # read in old obs data
-old_obs = read.csv(file = paste(dir.archive, "vw_obs.csv", sep="/"), header=TRUE, quote="", row.names=NULL, stringsAsFactors = FALSE)
-# assuming this is an export error?
-old_obs$observation_id = as.numeric(as.character(old_obs$observation_id))
-old_obs = old_obs[!is.na(old_obs$observation_id),]
-old_obs$transect_id = as.numeric(as.character(old_obs$transect_id))
-old_obs$source_obs_id = as.numeric(as.character(old_obs$source_obs_id))
-old_obs$obs_count_intrans_nb = as.numeric(as.character(old_obs$obs_count_intrans_nb))
-old_obs$obs_count_general_nb = as.numeric(as.character(old_obs$obs_count_general_nb))
-old_obs$angle_from_observer_nb = as.numeric(as.character(old_obs$angle_from_observer_nb))
-old_obs$seastate_beaufort_nb = as.numeric(as.character(old_obs$seastate_beaufort_nb))
-old_obs$seasurface_tempc_nb = as.numeric(as.character(old_obs$seasurface_tempc_nb))
-old_obs$who_created = as.numeric(as.character(old_obs$who_created))
-old_obs$temp_lat = as.numeric(as.character(old_obs$temp_lat))
-old_obs$temp_lon = as.numeric(as.character(old_obs$temp_lon))
-old_obs$who_imported = as.numeric(as.character(old_obs$who_imported))
-old_obs$salinity_ppt_nb = as.numeric(as.character(old_obs$salinity_ppt_nb))
-old_obs$local_obs_id = as.numeric(as.character(old_obs$local_obs_id))
-old_obs$heading_tx = as.character(old_obs$heading_tx)
-old_obs = old_obs[!is.na(old_obs$dataset_id),]
+old_obs = read.csv(file = paste(dir.archive, "vw_obs.csv", sep="/"), header=TRUE, quote="", row.names=NULL, stringsAsFactors = FALSE) %>% 
+  mutate(observation_id = as.numeric(as.character(observation_id))) %>% filter(!is.na(observation_id)) %>%
+  mutate(transect_id = as.numeric(as.character(transect_id)),
+         source_obs_id = as.numeric(as.character(source_obs_id)),
+         obs_count_intrans_nb = as.numeric(as.character(obs_count_intrans_nb)),
+         obs_count_general_nb = as.numeric(as.character(obs_count_general_nb)),
+         angle_from_observer_nb = as.numeric(as.character(angle_from_observer_nb)),
+         seastate_beaufort_nb = as.numeric(as.character(seastate_beaufort_nb)),
+         seasurface_tempc_nb = as.numeric(as.character(seasurface_tempc_nb)),
+         who_created = as.numeric(as.character(who_created)),
+         temp_lat = as.numeric(as.character(temp_lat)),
+         temp_lon = as.numeric(as.character(temp_lon)),
+         who_imported = as.numeric(as.character(who_imported)),
+         salinity_ppt_nb = as.numeric(as.character(salinity_ppt_nb)),
+         local_obs_id = as.numeric(as.character(local_obs_id)),
+         heading_tx = as.character(heading_tx)) %>% 
+         filter(!is.na(dataset_id))
 # ------------------------ #
 
 
@@ -92,7 +88,8 @@ rm(old_obs, obs.in.db)
 # ------------------------ #
 # pull FWS data
 # ------------------------ #
-obs_data = obs_data %>% filter(dataset_id %in% c(141, 142, 164, 118, 140, 138, 137, 139, 100, 97, 147, 146, 113, 106))
+obs_data = filter(obs_data, dataset_id %in% c(141, 142, 164, 118, 140, 138, 137, 139, 147, 146, 113 ))
+data.in.db = filter(data.in.db, dataset_id %in% c(141, 142, 164, 118, 140, 138, 137, 139, 147, 146, 113 ))
 # ------------------------ #
 
 # ------------------------ #
@@ -116,10 +113,24 @@ obs_data = left_join(obs_data, dplyr::select(spplist,-species_type_cd), by="spp_
 # ------------------------ #
 # change names
 # ------------------------ #
-obs_data = obs_data %>% rename(longitude = temp_lon, latitude = temp_lat, itis_tsn = itis_cd,
-                               observation_date = obs_dt) %>% 
-  dplyr::select(latitude, longitude, observation_date, itis_tsn, common_name, scientific_name) %>% 
-  mutate(count_type = "individual observation", centroid = "no", year, month, day, time = NA)
+obs_data$obs_dt[grepl("/", obs_data$obs_dt)] = as.character(as.Date(obs_data$obs_dt[grepl("/", obs_data$obs_dt)],"%m/%d/%Y")) # skrewy dates
+obs_data = obs_data %>% 
+  mutate(individual_count = ifelse(!is.na(obs_count_general_nb),obs_count_general_nb,obs_count_intrans_nb)) %>% 
+  rename(longitude = temp_lon, latitude = temp_lat, itis_tsn = itis_cd,
+         observation_date = obs_dt, collector=observers_tx, project_id = source_dataset_id,
+         GlobalID = observation_id, provided_common_name = common_name) %>% 
+  dplyr::select(latitude, longitude, observation_date, itis_tsn, provided_common_name, scientific_name,
+                individual_count, collector, source_transect_id, project_id, GlobalID) %>% rowwise %>% 
+  mutate(count_type = "individual observation", centroid = "no", 
+         observation_date = as.Date(observation_date,"%Y-%m-%d"),
+         year = format(observation_date, "%Y"), 
+         month = format(observation_date, "%m"), 
+         day = format(observation_date, "%d"), 
+         time = NA, documentation_link="https://connect.doi.gov/fws/Portal/acjv/seabird/SitePages/Home.aspx",
+         individual_count = ifelse(is.na(individual_count) & provided_common_name %in% "boat--trawler/dragger",1,individual_count),
+         collector = ifelse(is.na(collector),sapply(strsplit(source_transect_id,"_"),tail,1),collector),
+         location_name = "Northwest Atlantic Ocean within the U.S. EEZ") %>%
+  dplyr::select(-source_transect_id) 
 # ------------------------ #
 
 
@@ -127,24 +138,5 @@ obs_data = obs_data %>% rename(longitude = temp_lon, latitude = temp_lat, itis_t
 # export csv
 # ------------------------ #
 write.csv(obs_data, paste(dir.out, "observations.csv", sep="/"), row.names=FALSE)
-write.csv(data.in.db, paste(dir.out, "datasets.csv", sep="/"), row.names=FALSE)
-
-# remove NA's
-obs_data = obs_data[!is.na(obs_data$latitude),]
-obs_data = obs_data[!is.na(obs_data$longitude),]
+#write.csv(data.in.db, paste(dir.out, "datasets.csv", sep="/"), row.names=FALSE)
 # ------------------------ #
-
-
-# ------------------------ #
-# make shapefiles
-# ------------------------ #
-# Coerce into SpatialPointsDataframe
-coordinates(obs_data) = ~longitude + latitude
-
-# Assign projection
-proj4string(obs_data)=CRS("+proj=longlat +datum=WGS84") 
-
-# write shapefile
-writeOGR(obj=obs_data, dsn=dir.out, layer="all_obs_data", driver="ESRI Shapefile") 
-# ------------------------ #
-
