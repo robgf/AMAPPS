75bad3534aba0dec91424e856a07f070796b86d0
Kaycee Coleman <kaycee.coleman@gmail.com>
2017-10-12 16:57:57 -0400

    Update ME2016.R

--- NWASC/cleaning/Maine_gov/ME2016.R
+++ NWASC/cleaning/Maine_gov/ME2016.R
@@ -62,12 +62,86 @@ tn = data %>% dplyr::select(TransectID,TransectStarted,TransectEnded,
   arrange(ObservationTimestamp) %>% rename(endLon=GPSLongitude,
                                       endLat=GPSLatitude,
                                       lastObservation = ObservationTimestamp)
-transects = full_join(t1,dplyr::select(tn,TransectID,endLat,endLon,lastObservation),by="TransectID")
+# estimated transect start and end locations
+est.transects = full_join(t1,dplyr::select(tn,TransectID,endLat,endLon,lastObservation),by="TransectID")
 rm(t1,tn)
+
+# only looking at known transect start and stop times
+transects = dplyr::select(est.transects,TransectID,TransectStarted,TransectEnded) %>% 
+  rename(source_transect_id = TransectID) %>% 
+  mutate(start_tm = sapply(strsplit(as.character(TransectStarted)," "),tail,1),
+         end_tm = sapply(strsplit(as.character(TransectEnded)," "),tail,1), 
+         start_dt = sapply(strsplit(as.character(TransectStarted)," "),head,1), 
+         end_dt = sapply(strsplit(as.character(TransectEnded)," "),head,1)) %>% 
+  dplyr::select(-TransectStarted,-TransectEnded)
 
 # missing end times for these transects
 # c(2,6,9,14,19,23,29,33,45,46,48,52,56,59,61,64,65,68,69,71,73,75,77,79,80,82,84,91)
 # ------------- #
+
+
+# ------------- #
+# define species codes off itis #, check that common names are the same
+# ------------- #
+# ------------- #
+
+
+# ------------- #
+# define behavior,age ids
+# ------------- #
+#Behavior
+data = mutate(data, age_id = Age,
+              age_id = replace(age_id,"Carrying fish",16),
+              age_id = replace(age_id,"Dead",6),
+              age_id = replace(age_id,"Diving",7),
+              age_id = replace(age_id,"Feeding",9),
+              age_id = replace(age_id,"Flying",13),
+              age_id = replace(age_id,"Following",14),
+              age_id = replace(age_id,"Kleptoparasitizing",24),
+              age_id = replace(age_id,"Milling",21),
+              age_id = replace(age_id,c("Taking Off","Pattering"),42),
+              age_id = replace(age_id,"Plunge Diving",8),
+              age_id = replace(age_id,"Sitting",35))            
+
+#Age
+"Adult"=1       
+"Immature"=6    
+"Juvenile"=2    
+"subadult"=7    
+"Third year","Fifth year","First cycle","First year","Fourth year"  
+
+#Sex
+"Female"=1  
+"Male"=2    
+"Unknown"=5
+# ------------- #
+
+
+# ------------- #
+# rename
+# ------------- #
+data = data %>% 
+  mutate(obs_dt=sapply(strsplit(as.character(ObservationTimestamp)," "),head,1),
+         obs_tm=,sapply(strsplit(as.character(ObservationTimestamp)," "),tail,1))
+  rename(source_observation_id=ObservationID,
+         source_transect_id=TransectID,
+         obs_lat = GPSLatitude,
+         obs_lon = GPSLongitude,
+         original_species_tx = paste(SpeciesCode,SpeciesCommonName,SpeciesITISNumber,sep=" ; "),
+         obs_count_intrans_nb = Count,
+         observer_tx = Observer,
+         observer_position = Position,
+         plumage_tx = Plumage,
+         heading_tx = GPSHeading_degrees,
+         distance_to_animal_tx = paste(distance_m,"meter(s)",sep=" "),
+         angle_from_observer_nb = Degree,
+         associations_tx = LinkedwithObservationID,
+         visibility_tx = Visibility,
+         seastate_beaufort_nb = BeaufortID,
+         cloud_cover_tx = Weather,
+         observer_comments = paste(ObservationComment,BehaviorDescription,sep=" ; "))
+# ------------- #
+
 
 # ------------- #
 # export
