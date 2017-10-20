098d40c28517345841fcb426928d283104ef83af
Kaycee Coleman <kaycee.coleman@gmail.com>
2017-10-20 15:16:01 -0400

    Update ME2016.R

--- NWASC/cleaning/Maine_gov/ME2016.R
+++ NWASC/cleaning/Maine_gov/ME2016.R
@@ -50,13 +50,13 @@ colnames(data)[37]="GPSAltitude_m" #"GPSAltitude(m)"
 # ------------- #
 # summarize estimated transect info
 # ------------- #
-t1 = data %>% dplyr::select(TransectID,TransectStarted,TransectEnded,
+t1 = data %>% dplyr::select(TransectID,TransectStarted,TransectEnded,TransectComments,TransectName,
                                    GPSLongitude,GPSLatitude,ObservationTimestamp) %>% 
   group_by(TransectID) %>% filter(row_number()==1) %>% 
   arrange(ObservationTimestamp) %>% rename(startLon=GPSLongitude,
                                       startLat=GPSLatitude,
                                       firstObservation = ObservationTimestamp)
-tn = data %>% dplyr::select(TransectID,TransectStarted,TransectEnded,
+tn = data %>% dplyr::select(TransectID,TransectStarted,TransectEnded,TransectComments,TransectName,
                             GPSLongitude,GPSLatitude,ObservationTimestamp) %>% 
   group_by(TransectID) %>% filter(row_number()==n()) %>% 
   arrange(ObservationTimestamp) %>% rename(endLon=GPSLongitude,
@@ -67,8 +67,8 @@ est.transects = full_join(t1,dplyr::select(tn,TransectID,endLat,endLon,lastObser
 rm(t1,tn)
 
 # only looking at known transect start and stop times
-transects = dplyr::select(est.transects,TransectID,TransectStarted,TransectEnded) %>% 
-  rename(source_transect_id = TransectID) %>% 
+transects = dplyr::select(est.transects,TransectID,TransectStarted,TransectEnded,TransectName,TransectComments) %>% 
+  rename(source_transect_id = TransectID,comments=TransectComments) %>% 
   mutate(start_tm = sapply(strsplit(as.character(TransectStarted)," "),tail,1),
          end_tm = sapply(strsplit(as.character(TransectEnded)," "),tail,1), 
          start_dt = sapply(strsplit(as.character(TransectStarted)," "),head,1), 
@@ -81,8 +81,16 @@ transects = dplyr::select(est.transects,TransectID,TransectStarted,TransectEnded
 
 
 # ------------- #
-# define species codes off itis #, check that common names are the same
+# define species codes off itis 
+# check that common names are the same
 # ------------- #
+mutate(data,sort(unique(SpeciesCode[is.na(SpeciesITISNumber)])))
+
+db <- odbcDriverConnect('driver={SQL Server}; server=ifw-dbcsqlcl1.fws.doi.net; database=NWASC; trusted_connection=true')
+spplist <- sqlFetch(db, "lu_species")
+odbcClose(db)
+data = left_join(data, dplyr::select(spplist,spp_cd,ITIS_id), by=c("SpeciesITISNumber"="ITIS_id")) %>%
+  mutate(replace)
 # ------------- #
 
 
@@ -90,30 +98,35 @@ transects = dplyr::select(est.transects,TransectID,TransectStarted,TransectEnded
 # define behavior,age ids
 # ------------- #
 #Behavior
-data = mutate(data, age_id = Age,
-              age_id = replace(age_id,"Carrying fish",16),
-              age_id = replace(age_id,"Dead",6),
-              age_id = replace(age_id,"Diving",7),
-              age_id = replace(age_id,"Feeding",9),
-              age_id = replace(age_id,"Flying",13),
-              age_id = replace(age_id,"Following",14),
-              age_id = replace(age_id,"Kleptoparasitizing",24),
-              age_id = replace(age_id,"Milling",21),
-              age_id = replace(age_id,c("Taking Off","Pattering"),42),
-              age_id = replace(age_id,"Plunge Diving",8),
-              age_id = replace(age_id,"Sitting",35))            
+data = mutate(data, behavior_id = as.character(Behavior),
+              behavior_id = replace(behavior_id, behavior_id %in% "Carrying fish", 16),
+              behavior_id = replace(behavior_id, behavior_id %in% "Dead", 6),
+              behavior_id = replace(behavior_id, behavior_id %in% "Diving", 7),
+              behavior_id = replace(behavior_id, behavior_id %in% "Feeding", 9),
+              behavior_id = replace(behavior_id, behavior_id %in% "Flying", 13),
+              behavior_id = replace(behavior_id, behavior_id %in% "Following", 14),
+              behavior_id = replace(behavior_id, behavior_id %in% "Kleptoparasitizing", 24),
+              behavior_id = replace(behavior_id, behavior_id %in% "Milling", 21),
+              behavior_id = replace(behavior_id, behavior_id %in% c("Taking Off","Pattering"), 42),
+              behavior_id = replace(behavior_id, behavior_id %in% "Plunge Diving", 8),
+              behavior_id = replace(behavior_id, behavior_id %in% "Sitting", 35),
+              behavior_id = replace(behavior_id, is.na(behavior_id),44))            
 
 #Age
-"Adult"=1       
-"Immature"=6    
-"Juvenile"=2    
-"subadult"=7    
-"Third year","Fifth year","First cycle","First year","Fourth year"  
+data = mutate(data, age_id = as.character(Age),
+              age_id = replace(age_id, age_id %in% "Adult",1),
+              age_id = replace(age_id, age_id %in% "Immature",6),
+              age_id = replace(age_id, age_id %in% "Juvenile",2),
+              age_id = replace(age_id, age_id %in% "subadult",7),
+              age_id = replace(age_id, age_id %in% c("Third year","Fifth year","First cycle","First year","Fourth year"),4),
+              age_id = replace(age_id, is.na(age_id),5))  
 
 #Sex
-"Female"=1  
-"Male"=2    
-"Unknown"=5
+data = mutate(data, sex_id = as.character(Sex),
+              sex_id = replace(sex_id, sex_id %in% "Female",1), 
+              sex_id = replace(sex_id, sex_id %in% "Male",2),
+              sex_id = replace(sex_id, sex_id %in% "Unknown",5),
+              sex_id = replace(sex_id, is.na(sex_id),5)) 
 # ------------- #
 
 
@@ -122,24 +135,32 @@ data = mutate(data, age_id = Age,
 # ------------- #
 data = data %>% 
   mutate(obs_dt=sapply(strsplit(as.character(ObservationTimestamp)," "),head,1),
-         obs_tm=,sapply(strsplit(as.character(ObservationTimestamp)," "),tail,1))
-  rename(source_observation_id=ObservationID,
+         obs_tm=sapply(strsplit(as.character(ObservationTimestamp)," "),tail,1),
+         original_species_tx = paste(SpeciesCode,SpeciesCommonName,SpeciesITISNumber,sep=" ; "),
+         distance_to_animal_tx = paste(distance_m,"meter(s)",sep=" "),
+         observer_comments = paste(ObservationComment,BehaviorDescription,sep=" ; ")) %>% 
+  rename(source_obs_id=ObservationID,
          source_transect_id=TransectID,
          obs_lat = GPSLatitude,
          obs_lon = GPSLongitude,
-         original_species_tx = paste(SpeciesCode,SpeciesCommonName,SpeciesITISNumber,sep=" ; "),
          obs_count_intrans_nb = Count,
          observer_tx = Observer,
          observer_position = Position,
          plumage_tx = Plumage,
          heading_tx = GPSHeading_degrees,
-         distance_to_animal_tx = paste(distance_m,"meter(s)",sep=" "),
          angle_from_observer_nb = Degree,
          associations_tx = LinkedwithObservationID,
          visibility_tx = Visibility,
          seastate_beaufort_nb = BeaufortID,
          cloud_cover_tx = Weather,
-         observer_comments = paste(ObservationComment,BehaviorDescription,sep=" ; "))
+         original_age_tx = Age,
+         original_behavior_tx = Behavior,
+         original_sex_tx = Sex) %>%
+  dplyr::select(-TransectStarted,-TransectEnded,-TransectComments,
+                -ObservationType,-SpeciesCommonName,
+                -SpeciesITISNumber,-BehaviorDescription,
+                -distance_m,-ObservationComment,-GPSTimestamp,
+                -TransectName,-SpeciesCode)
 # ------------- #
 
 
