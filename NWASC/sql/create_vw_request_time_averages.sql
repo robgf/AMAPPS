008723669bd5a970a1c7b040d52f0c099f3ef72a
Kaycee Coleman <kaycee.coleman@gmail.com>
2017-12-27 15:33:50 -0500

    Create create_vw_request_time_averages.sql

---
+++ NWASC/sql/create_vw_request_time_averages.sql
@@ -0,0 +1,18 @@
+* find the average number of days it takes to fill a request */
+
+-- set database
+use NWASC;
+
+-- look at request table
+-- select * from requests where request_status = 'filled' 
+
+-- create view with datediff and type
+create view [request_time_averages] as
+select DATEDIFF(day, date_requested, date_filled) as 'duration',
+request_type from requests
+where request_status = 'filled' 
+and request_id <> 1
+
+-- determine the average days per type
+select request_type, avg(duration) as 'days' from [request_time_averages]
+group by request_type
