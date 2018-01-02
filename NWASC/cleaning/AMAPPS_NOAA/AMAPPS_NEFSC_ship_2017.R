ce4282f26ae5e6feb04fd0f60fa15c7f6bb41bd4
Kaycee Coleman <kaycee.coleman@gmail.com>
2018-01-02 17:36:09 -0500

    Update AMAPPS_NEFSC_ship_2017.R

--- NWASC/cleaning/AMAPPS_NOAA/AMAPPS_NEFSC_ship_2017.R
+++ NWASC/cleaning/AMAPPS_NOAA/AMAPPS_NEFSC_ship_2017.R
@@ -16,6 +16,9 @@ require(zoo)
 require(dplyr)
 library(readxl)
 library(odbc)
+library(ggmap)
+library(maps)
+library(mapdata)
 # --------------------- #
 
 
@@ -24,13 +27,18 @@ library(odbc)
 # --------------------- #
 obs <- read_excel("Z:/seabird_database/datasets_received/NOAA NMFS/NEFSC_AMAPPS_ship_2017/HRS1701SeabirdSightExport.xlsx")
 track <- read_excel("Z:/seabird_database/datasets_received/NOAA NMFS/NEFSC_AMAPPS_ship_2017/HRS1701SeabirdEffortExport.xlsx")
+
+usa <- map_data("usa")
 # --------------------- #
 
 
 # --------------------- #
 # fix errors
 # --------------------- #
-# check species
+
+# ------- #
+# fix species
+# ------- #
 db <- dbConnect(odbc::odbc(), driver='SQL Server',server='ifw-dbcsqlcl1',database='NWASC')
 spplist = dbGetQuery(db,"select * from lu_species")
 
@@ -38,13 +46,44 @@ tmp <- !obs$SPECIES %in% spplist$spp_cd
 message("Found ", sum(tmp), " entries with non-matching AOU codes")
 sort(unique(obs$SPECIES[tmp]))
 
-#obs$SPECIES[obs$SPECIES %in% "AGPL"] = "" 
-#obs$SPECIES[obs$SPECIES %in% "AMRS"] = "" 
-#obs$SPECIES[obs$SPECIES %in% "CEWA"] = "" 
-#obs$SPECIES[obs$SPECIES %in% "GCFC"] = "" 
+obs$SPECIES[obs$SPECIES %in% "AGPL"] = "AMGP" 
+obs$SPECIES[obs$SPECIES %in% "AMRS"] = "" 
+obs$SPECIES[obs$SPECIES %in% "CEWA"] = "" 
+obs$SPECIES[obs$SPECIES %in% "GCFC"] = "" #Myiarchus crinitus
 
+obs$SPECIES[obs$SPECIES %in% "PLOV" & obs$COMMENTS %in% c("likely semipalmated plover")] = "SEPL" 
+obs$SPECIES[obs$SPECIES %in% "PLOV" & obs$COMMENTS %in% c("possibly PIPL")] = "PIPL" 
+obs$SPECIES[obs$SPECIES %in% "PLOV"] = "CHAR" 
 obs$SPECIES[obs$SPECIES %in% "DUCK"] = "UNDU" 
 obs$SPECIES[obs$SPECIES %in% "PASS" & obs$COMMENTS %in% c("unid warbler","warbler sp.")] = "UNWA" 
 obs$SPECIES[obs$SPECIES %in% "PASS" & obs$COMMENTS %in% c("likely brown-headed cowbird")] = "BHCO" 
 obs$SPECIES[obs$SPECIES %in% "PASS"] = "UNPA" 
-#obs$SPECIES[obs$SPECIES %in% "PLOV"] = "UNPL" 
+# ------- #
+
+# ------- #
+# fix time
+# ------- #
+# split time and 
+obs = mutate(obs, data = SIGHTTIMEDATELOCAL, time = data = SIGHTTIMEDATELOCAL) %>% 
+  dplyr::select(-SIGHTTIMEDATELOCAL)
+track = mutate(track, data = TIMEDATELOCAL, time = data = TIMEDATELOCAL) %>% 
+  dplyr::select(-TIMEDATELOCAL)
+# ------- #
+
+# ------- #
+# add transects to obs
+# ------- #
+# join obs and transect
+names(obs)=tolower(names(obs))
+obs = obs %>% rename(type = species)
+names(track)=tolower(names(track))
+track$type = "WAYPNT"
+
+obstrack = bind.rows(obs,track) %>% mutate(transect = na.locf(transect))
+# ------- #
+
+# plots
+ggplot(data =usa) + geom_polygon(aes(x = long, y = lat, group = group), fill = "#669933", color = "#333300") + 
+  coord_fixed(1.3) + #coord_fixed(xlim = c(-82, -65),  ylim = c(25, 47), ratio = 1.3) + 
+  coord_fixed(xlim = c(-75, -67),  ylim = c(38, 42), ratio = 1.3) + 
+  geom_point(data = obs, aes(x = LON, y = LAT)) + theme_bw()
