18e2fa87f0485ecf25881e74e485335cbe06048b
Kaycee Coleman <kaycee.coleman@gmail.com>
2018-01-30 17:42:11 -0500

    Update postObsEdits.R

--- cleaning/AMAPPS_2017_08/postObsEdits.R
+++ cleaning/AMAPPS_2017_08/postObsEdits.R
@@ -46,16 +46,59 @@ obstrack$offline[obstrack$key %in% 'Crew3521_rf_2017_8_20' & is.na(obstrack$obs)
 # -75.09000, 36.85 #east -> need to add
 to.add = as.data.frame(matrix(nrow=1,data = c(-75.77895, 36.85, NA, NA, 8, 26, 2017, NA, NA, "Added split between 365100/365101", NA)))
 names(to.add) = c("long","lat","obs","seat","month","day","year","transect","type","comment", "key")
-to.add1 = to.add %>% mutate(obs = 'jfv', seat = 'rf', transect = 365101, type = 'ENDCNT', index = 258.1, key = 'Crew4126_rf_2017_8_26',
-                            long = as.numeric(long),lat = as.numeric(lat), month=as.numeric(month), day=as.numeric(day), year = as.numeric(year))
-to.add2 = to.add %>% mutate(obs = 'jfv', seat = 'rf', transect = 365100, type = 'BEGCNT', index = 258.9, key = 'Crew4126_rf_2017_8_26',
-                            long = as.numeric(long),lat = as.numeric(lat), month=as.numeric(month), day=as.numeric(day), year = as.numeric(year))
-to.add3 = to.add %>% mutate(obs = 'sde', seat = 'lf', transect = 365101, type = 'ENDCNT', index = 276.1, key = 'Crew4126_lf_2017_8_26',
-                            long = as.numeric(long),lat = as.numeric(lat), month=as.numeric(month), day=as.numeric(day), year = as.numeric(year))
-to.add4 = to.add %>% mutate(obs = 'sde', seat = 'lf', transect = 365100, type = 'BEGCNT', index = 276.9, key = 'Crew4126_lf_2017_8_26',
-                            long = as.numeric(long),lat = as.numeric(lat), month=as.numeric(month), day=as.numeric(day), year = as.numeric(year))
+to.add1 = to.add %>% 
+  mutate(obs = 'jfv', 
+         seat = 'rf', 
+         transect = 365101, 
+         type = 'ENDCNT', 
+         index = 258.1, 
+         key = 'Crew4126_rf_2017_8_26',
+         long = as.numeric(as.character(long)),
+         lat = as.numeric(as.character(lat)), 
+         month=as.numeric(as.character(month)), 
+         day=as.numeric(as.character(day)), 
+         year = as.numeric(as.character(year)))
+to.add2 = to.add %>% 
+  mutate(obs = 'jfv', 
+         seat = 'rf', 
+         transect = 365100, 
+         type = 'BEGCNT', 
+         index = 258.9, 
+         key = 'Crew4126_rf_2017_8_26',
+         long = as.numeric(as.character(long)),
+         lat = as.numeric(as.character(lat)), 
+         month=as.numeric(as.character(month)), 
+         day=as.numeric(as.character(day)), 
+         year = as.numeric(as.character(year)))
+to.add3 = to.add %>% 
+  mutate(obs = 'sde', 
+         seat = 'lf', 
+         transect = 365101, 
+         type = 'ENDCNT', 
+         index = 276.1, 
+         key = 'Crew4126_lf_2017_8_26',
+         long = as.numeric(as.character(long)),
+         lat = as.numeric(as.character(lat)), 
+         month=as.numeric(as.character(month)), 
+         day=as.numeric(as.character(day)), 
+         year = as.numeric(as.character(year)))
+to.add4 = to.add %>% 
+  mutate(obs = 'sde', 
+         seat = 'lf', 
+         transect = 365100, 
+         type = 'BEGCNT', 
+         index = 276.9, 
+         key = 'Crew4126_lf_2017_8_26',
+         long = as.numeric(as.character(long)),
+         lat = as.numeric(as.character(lat)), 
+         month=as.numeric(as.character(month)), 
+         day=as.numeric(as.character(day)), 
+         year = as.numeric(as.character(year)))
 obstrack = bind_rows(obstrack, to.add1, to.add2, to.add3, to.add4) %>% arrange(key,sec,index)
 rm(to.add,to.add1,to.add2,to.add3,to.add4)
+
+obstrack$transect[obstrack$transect %in% 365101 & obstrack$sec %in% c(58052.27,57854.75)] = 365100
+
 
 # Crew4126_lf_2017_8_27
 # 363100 needs and BEGCNT; 302100 need to remove BEGCOUNT that changed from 363100 to 363600
